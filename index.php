5c8985e732d8d33de5d4e3063c0e0d0aa932100d
Sascha-Darius Knießner <dariuskniessner@littlewebgiants.com>
2018-01-20 19:41:10 +0100

    v

--- index.php
+++ index.php
@@ -143,5 +143,4 @@ get_header(); ?>
       echo wp_get_attachment_image( $random_attachments[1]->ID, 'large', "", array( "class" => "background_image screen_fit" ) );
     }
    ?>
-<?php
-get_footer();
+<?php get_footer(); ?>
